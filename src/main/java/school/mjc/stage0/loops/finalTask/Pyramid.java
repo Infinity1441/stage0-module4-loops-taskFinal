fdf454995922ee661e6be756ca16536e8c330809
Infinity1441 <jumabekova.baxtigul@gmail.com>
2022-08-19 19:42:39 +0500

    t

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,31 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int i, j, k, n, a;
 
-    }
+        n = cathetusLength;
+        a = n;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = a; j > 1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (k = i; k != 0; k--)
+            {
+                System.out.print(k);
 
-    public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+            }
+            a--;
+
+            for (int l = 2; l <= i; l++)
+            {
+                System.out.print(l);
+
+            }
+            System.out.println();
+        }
+
     }
 }
+
